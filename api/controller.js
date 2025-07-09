b492ec938430f5cbef7d83932ef1de27481a2388
shreya <70858293+alt-shreya@users.noreply.github.com>
2025-07-09 14:10:48 +0000

    Add pow operation to calculator operations

--- api/controller.js
+++ api/controller.js
@@ -17,6 +17,7 @@ exports.calculate = function(req, res) {
     'multiply': function(a, b) { return a * b },
     'divide':   function(a, b) { return a / b },
     'power':    function(a, b) { return Math.pow(Number(a), Number(b)) }
+    // 'modulus':  function(a, b) { return a % b } // Uncomment if modulus is needed
   };
 
   if (!req.query.operation) {
